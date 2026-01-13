75d336a73ddd302b41170bfb84c283b9008704a9
goyalpramod <goyalpramod1729@gmail.com>
2026-01-13 14:19:41 +0530

    week3: setup screens

--- week_3/8bitme/src/App.tsx
+++ week_3/8bitme/src/App.tsx
@@ -2,6 +2,7 @@ import Fun from './pages/Fun';
 import Wild from './pages/Wild';
 import Projects from './pages/Projects';
 import Hub from './pages/Hub';
+import Start from './pages/Start';
 import About from './pages/About';
 import { BrowserRouter, Routes, Route } from "react-router";
 
@@ -9,7 +10,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hub />} />
+        <Route path="/" element={<Start />} />
+        <Route path="/hub" element={<Hub />} />
         <Route path='/about' element={<About />} />
         <Route path='/fun' element={<Fun />} />
         <Route path='/projects' element={<Projects />} />
--- week_3/8bitme/src/pages/Start.tsx
+++ week_3/8bitme/src/pages/Start.tsx
@@ -2,14 +2,12 @@ import ground from '../assets/elements/ground.png'
 import sun from '../assets/elements/sun.png'
 import cloud2 from '../assets/elements/cloud_2.png'
 import character from '../assets/character/char1.png'
-import door from '../assets/elements/door_1.png'
 
-const Hub = () => {
+const Start = () => {
     return (
     <div className="" style={{ backgroundColor: '#79C8F2', position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}>
         <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(43%)'}} src={ground} alt=''/>
         <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(27%) scale(0.2)'}} src={character} alt=''/>
-        <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(27%) scale(0.2)'}} src={door} alt=''/>
         <img style={{ position: 'absolute',top: '0', right: '0'}} src={sun} alt=''/>
         <img style={{ position: 'absolute',top: '0', transform: 'translateY(43%)'}} src={cloud2} alt=''/>
         <img style={{ position: 'absolute',top: '0', transform: 'translateX(100%)'}} src={cloud2} alt=''/>
@@ -17,4 +15,4 @@ const Hub = () => {
     );
 }
 
-export default Hub
+export default Start
