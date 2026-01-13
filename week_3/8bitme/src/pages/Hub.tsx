c77c74eb457c6ec8aee15e4179e289be6c121062
goyalpramod <goyalpramod1729@gmail.com>
2026-01-13 14:22:15 +0530

    week3: setup initial screens

--- week_3/8bitme/src/pages/Hub.tsx
+++ week_3/8bitme/src/pages/Hub.tsx
@@ -9,7 +9,9 @@ const Hub = () => {
     <div className="" style={{ backgroundColor: '#79C8F2', position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}>
         <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(43%)'}} src={ground} alt=''/>
         <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(27%) scale(0.2)'}} src={character} alt=''/>
-        <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(27%) scale(0.2)'}} src={door} alt=''/>
+        <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(27%) translateX(40%) scale(0.2)'}} src={door} alt=''/>
+        <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(27%) translateX(80%) scale(0.2)'}} src={door} alt=''/>
+        <img style={{ position: 'absolute',bottom: '0', transform: 'translateY(27%) translateX(100%) scale(0.2)'}} src={door} alt=''/>
         <img style={{ position: 'absolute',top: '0', right: '0'}} src={sun} alt=''/>
         <img style={{ position: 'absolute',top: '0', transform: 'translateY(43%)'}} src={cloud2} alt=''/>
         <img style={{ position: 'absolute',top: '0', transform: 'translateX(100%)'}} src={cloud2} alt=''/>
